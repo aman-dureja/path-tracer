662e2361d6d201f8e4858cb2b299081a454124bc
Aman Dureja <dureja.aman.97@gmail.com>
2019-08-20 23:08:40 -0400

    Chapter 2: The Vec3 class
    Create data type Vec3 to encapsulate 3D vectors

--- Tracer.hs
+++ Tracer.hs
@@ -1,3 +1,5 @@
+import qualified Vec3 as V
+
 main :: IO ()
 main = do
     let nx = 200
@@ -10,12 +12,10 @@ main = do
             if i >= nx
                 then rgbRows (j - 1) 0 nx ny
             else 
-                let r = i / nx
-                    g = j / ny
-                    b = 0.2
-                    ir = floor $ 255.99 * r
-                    ig = floor $ 255.99 * g
-                    ib = floor $ 255.99 * b 
+                let col = V.Vec3 (i/nx, j/ny, 0.2)
+                    ir = floor $ 255.99 * (V.r col)
+                    ig = floor $ 255.99 * (V.g col)
+                    ib = floor $ 255.99 * (V.b col)
                 in show ir ++ " " ++ show ig ++ " " ++ show ib ++ "\n" ++ rgbRows j (i + 1) nx ny
         | otherwise = ""
 
---
+++ Vec3.hs
@@ -0,0 +1,64 @@
+module Vec3 where
+
+data Vec3 = Vec3 (Float, Float, Float)
+
+x :: Vec3 -> Float
+x (Vec3 (a,_,_)) = a
+
+y :: Vec3 -> Float
+y (Vec3 (_,a,_)) = a
+
+z :: Vec3 -> Float
+z (Vec3 (_,_,a)) = a
+
+r :: Vec3 -> Float
+r = x
+
+g :: Vec3 -> Float
+g = y
+
+b :: Vec3 -> Float
+b = z
+
+neg :: Vec3 -> Vec3
+neg (Vec3 (a,b,c)) = Vec3 (-1*a, -1*b, -1*c)
+
+atIndex :: Vec3 -> Int -> Maybe Float
+atIndex (Vec3 (a,_,_)) 0 = Just a
+atIndex (Vec3 (_,a,_)) 1 = Just a
+atIndex (Vec3 (_,_,a)) 2 = Just a
+atIndex _ _ = Nothing 
+
+add :: Vec3 -> Vec3 -> Vec3
+add (Vec3 (a1,b1,c1)) (Vec3 (a2,b2,c2)) = Vec3 (a1+a2, b1+b2, c1+c2)
+
+sub :: Vec3 -> Vec3 -> Vec3
+sub (Vec3 (a1,b1,c1)) (Vec3 (a2,b2,c2)) = Vec3 (a1-a2, b1-b2, c1-c2)
+
+vecMul :: Vec3 -> Vec3 -> Vec3
+vecMul (Vec3 (a1,b1,c1)) (Vec3 (a2,b2,c2)) = Vec3 (a1*a2, b1*b2, c1*c2)
+
+vecDiv :: Vec3 -> Vec3 -> Vec3
+vecDiv (Vec3 (a1,b1,c1)) (Vec3 (a2,b2,c2)) = Vec3 (a1/a2, b1/b2, c1/c2)
+
+scalarMul :: Vec3 -> Float -> Vec3
+scalarMul (Vec3 (a,b,c)) n = Vec3 (a*n, b*n, c*n)
+
+scalarDiv :: Vec3 -> Float -> Vec3
+scalarDiv (Vec3 (a,b,c)) n = Vec3 (a/n, b/n, c/n)
+
+squaredLength :: Vec3 -> Float
+squaredLength (Vec3 (a,b,c)) = a*a + b*b + c*c
+
+len :: Vec3 -> Float
+len v = sqrt $ squaredLength v
+
+unitVector :: Vec3 -> Vec3
+unitVector v = scalarDiv v (len v)
+
+dot :: Vec3 -> Vec3 -> Float
+dot (Vec3 (a1,b1,c1)) (Vec3 (a2,b2,c2)) = a1*a2 + b1*b2 + c1*c2
+
+cross :: Vec3 -> Vec3 -> Vec3
+cross (Vec3 (a1,b1,c1)) (Vec3 (a2,b2,c2)) = Vec3 (b1*c2 - c1*b2, -1*(a1*c2 - c1*a2), a1*b2 - b1*a2)
+
